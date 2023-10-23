ed21c06e847d25bbcb30b02f23c3aefb759b36cc
Wallas-Reis <wallas_reis@outlook.com.br>
2023-10-23 12:31:58 -0300

    flask

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from bd import select
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    result = select("select coluna2 from tabela")
+    
+    # Extrair o valor Decimal da tupla
+    value = result[0][0] if result and result[0] else None
+    
+    return render_template('index.html', variavel=value)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
--- bd.py
+++ bd.py
@@ -1,43 +1,22 @@
-import pyodbc
+import psycopg2
 
-# Defina os detalhes da conexão
-server = 'seu_servidor'  # Endereço do servidor PostgreSQL
-database = 'seu_banco_de_dados'  # Nome do banco de dados
-username = 'seu_usuario'  # Nome de usuário
-password = 'sua_senha'  # Senha
+# Configuração da conexão com o banco de dados
+url = "postgres://wisecoin_user:gT06xyldPRImhVYLO62zl4C1DuKIta0I@dpg-cko2l3e1101c73da4tq0-a.oregon-postgres.render.com/wisecoin"
 
-# Crie uma string de conexão
-connection_string = f'DRIVER={{PostgreSQL}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+# Exemplo de consulta
+def select(query):
+    # Conectar ao banco de dados
+    connection = psycopg2.connect(url)
 
-# Estabeleça a conexão
-conn = pyodbc.connect(connection_string)
+    # Criar um cursor
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
 
-# Crie um cursor para executar consultas SQL
-cursor = conn.cursor()
+    # Fechar o cursor e a conexão
+    cursor.close()
+    connection.close()
 
-# Exemplo de SELECT
-cursor.execute('SELECT * FROM sua_tabela')
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+    return result
 
-# Exemplo de INSERT
-novo_valor1 = 'Novo Valor 1'
-novo_valor2 = 'Novo Valor 2'
-cursor.execute("INSERT INTO sua_tabela (coluna1, coluna2) VALUES (?, ?)", novo_valor1, novo_valor2)
-conn.commit()  # Confirme a transação
 
-# Exemplo de UPDATE
-novo_valor = 'Novo Valor Atualizado'
-valor_condicao = 'Valor de Condição'
-cursor.execute("UPDATE sua_tabela SET coluna1 = ? WHERE coluna2 = ?", novo_valor, valor_condicao)
-conn.commit()  # Confirme a transação
-
-# Exemplo de DELETE
-valor_condicao = 'Valor a ser excluído'
-cursor.execute("DELETE FROM sua_tabela WHERE coluna = ?", valor_condicao)
-conn.commit()  # Confirme a transação
-
-# Feche o cursor e a conexão quando terminar
-cursor.close()
-conn.close()
